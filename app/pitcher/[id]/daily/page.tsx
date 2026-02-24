a6420a2c5bff5027fa243687b7b3384b1742f1bc
zackm1018003 <zackm1018003@gmail.com>
2026-02-24 00:14:33 -0500

    Update page.tsx

--- app/pitcher/[id]/daily/page.tsx
+++ app/pitcher/[id]/daily/page.tsx
@@ -578,7 +578,6 @@ export default function PitcherDailyPage({ params, searchParams }: DailyPageProp
                   </div>
                 ))}
                 )}
-              </div>
             </div>
 
            
